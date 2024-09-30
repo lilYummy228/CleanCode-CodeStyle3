eb7414fe49f6c011678925bf3f2f6625d6cf60db
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-30 21:13:49 +0500

    Добавьте файлы проекта.

---
+++ CleanCode&CodeStyle3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CleanCode_CodeStyle3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int armySize = 10;
+            int coinsCount = 10;
+            string name = "Vladislav";
+        }
+    }
+}
